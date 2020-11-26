c12dd3f3ab104cebba364f4a7b48f7429c5c86f7
Pietari Kaskela <pietari.kaskela@gmail.com>
2020-11-26 14:09:38 +0200

    Solver helpers

--- src/solver.cpp
+++ src/solver.cpp
@@ -139,7 +139,7 @@ std::vector<int> calculate_allowed_alpha_gpu(std::vector<std::vector<float>>& al
 }
 
 
-void solve_instance_gpu_alpha(std::vector<std::vector<float>> coords) {
+void solve_instance_gpu_alpha(std::vector<std::vector<float>> coords, const int MAX_EDGES2, bool opt1, bool opt2) {
     int permutations = 100;
     int n = coords[0].size();
     float best = std::numeric_limits<float>::max();
@@ -148,25 +148,28 @@ void solve_instance_gpu_alpha(std::vector<std::vector<float>> coords) {
     timer.start();
     auto pi = gpu_subgradient_opt_alpha(coords[0].data(), coords[1].data(), n);
     auto onetree = prim_onetree_edges(coords, pi);
-    const int MAX_EDGES = std::min(20, n-1);
+    const int MAX_EDGES = std::min(MAX_EDGES2, n-1);
     auto candidate_set = candidate_generation_alpha(coords, pi, onetree, MAX_EDGES);
     auto allowed = calculate_allowed_alpha_gpu(candidate_set);
     timer.stop();
-    std::cout << "GPU alpha preprocessing took " << timer.elapsedMilliseconds() << std::endl;
-    timer.start();
+    std::cout << "preprocessing: " << timer.elapsedMilliseconds() << std::endl;
+    Timer timer2;
+    timer2.start();
     while(permutations--) {
         auto [shuffled_coords, shuffled_id] = initial_tour_alpha(coords, candidate_set);
-        run_gpu_2opt_restricted(shuffled_coords[0].data(), shuffled_coords[1].data(), shuffled_id.data(), allowed.data(), n, MAX_EDGES);
-        run_gpu_2opt(shuffled_coords[0].data(), shuffled_coords[1].data(), n);
+        if (opt1)
+            run_gpu_2opt_restricted(shuffled_coords[0].data(), shuffled_coords[1].data(), shuffled_id.data(), allowed.data(), n, MAX_EDGES);
+        if (opt2)
+            run_gpu_2opt(shuffled_coords[0].data(), shuffled_coords[1].data(), n);
         float new_tour_cost = tour_cost(shuffled_coords);
         if (new_tour_cost < best) {
             best = new_tour_cost;
             best_coords = shuffled_coords;
         }
     }
-    timer.stop();
-    std::cout << 100 << " perms took " << timer.elapsedMilliseconds() << std::endl;
-    std::cout << "GPU (alpha) best was: " << best << std::endl; 
+    timer2.stop();
+    std::cout << "2-opt: " << timer2.elapsedMilliseconds() << std::endl;
+    std::cout << "result: " << best << std::endl; 
 }
 
 
@@ -197,7 +200,7 @@ std::vector<std::vector<int>> calculate_allowed_alpha(std::vector<std::vector<st
 }
 
 
-void solve_instance_cpu_alpha(std::vector<std::vector<float>> coords) {
+void solve_instance_cpu_alpha(std::vector<std::vector<float>> coords, const int MAX_EDGES2, bool opt1, bool opt2) {
     int permutations = 100;
     int n = coords[0].size();
     float best = std::numeric_limits<float>::max();
@@ -206,81 +209,39 @@ void solve_instance_cpu_alpha(std::vector<std::vector<float>> coords) {
     timer.start();
     auto pi = subgradient_opt_alpha(coords);
     auto onetree = prim_onetree_edges(coords, pi);
-    const int MAX_EDGES = std::min(20, n-1);
+    const int MAX_EDGES = std::min(MAX_EDGES2, n-1);
     auto candidate_set = candidate_generation_alpha(coords, pi, onetree, MAX_EDGES);
     auto allowed = calculate_allowed_alpha(candidate_set);
     timer.stop();
-    std::cout << "CPU alpha preprocessing took " << timer.elapsedMilliseconds() << std::endl;
+    std::cout << "preprocessing: " << timer.elapsedMilliseconds() << std::endl;
 
+    Timer timer2;
+    timer2.start();
     std::vector<int> id(n);
     for (int i= 0; i < n; ++i)
         id[i] = i;
-    timer.start();
     while(permutations--) {
         auto [shuffled_coords, shuffled_id] = initial_tour_alpha(coords, candidate_set);
-        auto [x, y, id2] = two_opt_best_restricted(shuffled_coords[0], shuffled_coords[1], shuffled_id, allowed);
-        shuffled_coords[0] = x;
-        shuffled_coords[1] = y;
-        auto [x2, y2] = two_opt_best(shuffled_coords[0], shuffled_coords[1]);
-        shuffled_coords[0] = x2;
-        shuffled_coords[1] = y2;
+        if (opt1) {
+            auto [x, y, id2] = two_opt_best_restricted(shuffled_coords[0], shuffled_coords[1], shuffled_id, allowed);
+            shuffled_coords[0] = x;
+            shuffled_coords[1] = y;
+        }
+        if (opt2) {
+            auto [x2, y2] = two_opt_best(shuffled_coords[0], shuffled_coords[1]);
+            shuffled_coords[0] = x2;
+            shuffled_coords[1] = y2;
+        }
         float new_tour_cost = tour_cost(shuffled_coords);
         if (new_tour_cost < best) {
             best = new_tour_cost;
             best_coords = shuffled_coords;
         }
     }
-    timer.stop();
-    std::cout << 100 << " perms took " << timer.elapsedMilliseconds() << std::endl;
-    std::cout << "CPU (alpha) best was: " << best << std::endl; 
+    timer2.stop();
+    std::cout << "2-opt: " << timer2.elapsedMilliseconds() << std::endl;
+    std::cout << "result: " << best << std::endl; 
 }
-
-
-std::vector<std::vector<int>> calculate_allowed_shortest(std::vector<std::vector<float>>& coords, const int MAX_EDGES) {
-    int n = coords[0].size();
-    std::vector<std::vector<std::pair<float, int>>> tmp(n);
-    std::vector<std::vector<int>> allowed(n);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (i != j)
-                tmp[i].push_back({distance(coords, i, j), j});
-        }
-        std::sort(tmp[i].begin(), tmp[i].end());
-        for (int j = 0; j < MAX_EDGES; ++j)
-            allowed[i].push_back(tmp[i][j].second);
-    } 
-    return allowed;
-}
-
-
-void solve_instance_cpu_shortest(std::vector<std::vector<float>> coords) {
-    int permutations = 1;
-    int n = coords[0].size();
-    Timer timer;
-    timer.start();
-    std::vector<int> id(n);
-    for (int i = 0; i < n; ++i)
-        id[i] = i;
-    float best = std::numeric_limits<float>::max();
-    std::vector<std::vector<float>> best_coords;
-    auto allowed = calculate_allowed_shortest(coords, std::min(80, n-1));
-    while(permutations--) {
-        shuffle_id(coords, id);
-        auto [x, y, id2] = two_opt_best_restricted(coords[0], coords[1], id, allowed);
-        coords[0] = x;
-        coords[1] = y;
-        id = id2;
-        float new_tour_cost = tour_cost(coords);
-        if (new_tour_cost < best) {
-            best = new_tour_cost;
-            best_coords = coords;
-        }
-    }
-    timer.stop();
-    std::cout << 100 << " perms took " << timer.elapsedMilliseconds() << std::endl;
-    std::cout << "CPU (shortest) best was: " << best << std::endl; 
-}
-
 
 void solve_instance_gpu_random(std::vector<std::vector<float>> coords) {
     int permutations = 100;
@@ -299,46 +260,37 @@ void solve_instance_gpu_random(std::vector<std::vector<float>> coords) {
         }
     }
     timer.stop();
-    std::cout << 100 << " perms took " << timer.elapsedMilliseconds() << std::endl;
-    std::cout << "GPU (random) best was: " << best << std::endl; 
-}
-
-
-void solve_instance_cpu_random(std::vector<std::vector<float>> coords) {
-    int permutations = 100;
-    int n = coords[0].size();
-    float best = std::numeric_limits<float>::max();
-    std::vector<std::vector<float>> best_coords;
-    Timer timer;
-    timer.start();
-    while(permutations--) {
-        shuffle(coords);
-        auto [x, y] = two_opt_best(coords[0], coords[1]);
-        coords[0] = x;
-        coords[1] = y;
-        float new_tour_cost = tour_cost(coords);
-        if (new_tour_cost < best) {
-            best = new_tour_cost;
-            best_coords = coords;
-        }
-    }
-    timer.stop();
-    std::cout << "100 perms took " << timer.elapsedMilliseconds() << std::endl;
-    std::cout << "CPU best was: " << best << std::endl; 
+    std::cout << "2-opt: " << timer.elapsedMilliseconds() << std::endl;
+    std::cout << "result: " << best << std::endl; 
 }
 
 int main(int argc, char** argv) {
     std::cout << std::setprecision(10);
-    int seed = 43;
     std::srand(time(nullptr));
 
     auto p = read_graph(argv[1]);
-    //int n = std::stoi(argv[1]);
-    //auto p = random_graph(n);
-
-    solve_instance_gpu_alpha(p);
-    solve_instance_cpu_alpha(p);
-    //solve_instance_cpu_shortest(p);
-    solve_instance_gpu_random(p);
-    //solve_instance_cpu_random(p);
+    std::string algo(argv[2]);
+    if (algo.compare("RAND") == 0) {
+        solve_instance_gpu_random(p);
+    } else {
+        std::string device(argv[3]);
+        auto candidates = std::stoi(argv[4]);
+        if (device.compare("GPU") == 0) {
+            if (algo.compare("ALPHA1") == 0) {
+                solve_instance_gpu_alpha(p, candidates, false, true);
+            } else if (algo.compare("ALPHA2") == 0) {
+                solve_instance_gpu_alpha(p, candidates, true, false);
+            } else if (algo.compare("ALPHA3") == 0) {
+                solve_instance_gpu_alpha(p, candidates, true, true);
+            }
+        } else if (device.compare("CPU") == 0){
+            if (algo.compare("ALPHA1") == 0) {
+                solve_instance_cpu_alpha(p, candidates, false, true);
+            } else if (algo.compare("ALPHA2") == 0) {
+                solve_instance_cpu_alpha(p, candidates, true, false);
+            } else if (algo.compare("ALPHA3") == 0) {
+                solve_instance_cpu_alpha(p, candidates, true, true);
+            }
+        }
+    }
 }
